e11608ac257a5f4734bc53092fa1c44297e1f9af
DCMFC <dayanecristine.caldeira@gmail.com>
2019-06-16 18:55:02 -0300

    Update Jenkinsfile_python.groovy

--- Jenkinsfile_python.groovy
+++ Jenkinsfile_python.groovy
@@ -10,6 +10,7 @@ node {
     {
         sh 'cd magazine_luiza/'
         sh 'virtualenv env && . env/bin/activate'
+        new File(System.getProperty("user.dir")).name
         sh 'cd magazine_luiza/'
         sh 'pip3 install --upgrade -r requirements.txt'
     }
