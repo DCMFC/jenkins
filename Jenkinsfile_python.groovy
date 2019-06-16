9a5815ff61b55da4594c25d9cf98cb1c3538020c
DCMFC <dayanecristine.caldeira@gmail.com>
2019-06-16 18:30:15 -0300

    build stage

--- Jenkinsfile_python.groovy
+++ Jenkinsfile_python.groovy
@@ -5,5 +5,11 @@ node {
         credentialsId: 'git_credential',
         url: 'https://github.com/DCMFC/employee-manager.git'
     }
+    
+    stage('Build')
+    {
+        sh 'cd employee-manager/magazine_luiza/'
+        sh 'virtualenv env && source env/bin/activate && pip3 install --upgrade -r requirements.txt'
+    }
    
 }
