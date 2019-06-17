0dd3822ed61832a21599fe3fd51c1f84d31915a9
DCMFC <dayanecristine.caldeira@gmail.com>
2019-06-17 14:27:14 -0300

    sonar

--- Jenkinsfile_python.groovy
+++ Jenkinsfile_python.groovy
@@ -17,5 +17,12 @@ node {
     {
         sh 'python3 magazine_luiza/manage.py test'
     }
-   290a06dcf86c95266f8ddb743e436f10e1f49e20
+    
+    stage('SonarQube') {
+        def scannerHome = tool 'SonarQubeScanner';
+        withSonarQubeEnv('http://127.0.0.1:9000') {
+            sh "${scannerHome}/bin/sonar-scanner"
+        }
+    }
+   
 }
