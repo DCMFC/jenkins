b656c178a7bac07fae258bdfe938234dae56ccb6
DCMFC <dayanecristine.caldeira@gmail.com>
2019-06-18 14:29:59 -0300

    Update Jenkinsfile_python.groovy

--- Jenkinsfile_python.groovy
+++ Jenkinsfile_python.groovy
@@ -15,9 +15,7 @@ node {
     
     stage('Test')
     {
-        sh 'virtualenv env && . env/bin/activate'
         sh 'python3 magazine_luiza/manage.py test'
-        sh 'nosetests --with-coverage --cover-xml --cover-package=magazine_luiza --with-xunit test'
     }
     
     stage('SonarQube') {
